38587cf8129b7d9a16fd4eba71431ccd965e170e
NhatPhong <106730411+nhatphongdev@users.noreply.github.com>
2023-11-04 17:04:23 +0700

    Add files via upload

---
+++ run.bat
@@ -0,0 +1,5 @@
+@ echo off
+cls
+color 2
+cls
+python voicecreate.py
---
+++ voicecreate.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import traceback
+import sys
+
+intents = discord.Intents.default()
+#Message content intent needs to be enabled in the developer portal for your chosen bot.
+intents.message_content = True
+
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+bot.remove_command("help")
+
+DISCORD_TOKEN = 'YOUR_BOT_TOKEN'
+
+initial_extensions = ['cogs.voice']
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+    for extension in initial_extensions:
+        try:
+            await bot.load_extension(extension)
+            print(f'Loaded {extension}')
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+bot.run(DISCORD_TOKEN)
